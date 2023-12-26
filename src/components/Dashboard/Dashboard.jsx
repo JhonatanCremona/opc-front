457cce83e973267d7eb0fed784652c37b0a22946
JhonatanFulguera <jfulguera@creminox.com>
2023-12-26 12:01:44 -0300

    dataLogin componente SensorTemp

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -6,44 +6,15 @@ import { useState, useEffect } from "react";
 
 
 export const Dashboard = () => {
-    const [ data, setData ]= useState({});
-    const API_OPC = "http://localhost:5005/opcua/listaValores";
-
-   
-    console.log(data);
-    useEffect(()=>{
-        async function getData() {
-                try {
-                const response = await axios.get(API_OPC);
-                    setData(response.data);
-                } catch (error) {
-                console.error(error);
-                }
-            }
-            const intervalId = setInterval(getData, 1000);
-
-    // Limpiar intervalo al desmontar el componente
-    return () => clearInterval(intervalId);
-
-    },[])  
+    
 
     return(
         <div>
             <h1>Title</h1>
             <div>
                 <SensorTemp/>
-                <SensorTemp/>
-                <SensorTemp/>
             </div>
-            <div>
-            {data && (
-                        <ul>
-                        {data.map((item, index) => (
-                            <li key={index}>{item.data}</li>
-                        ))}
-                        </ul>
-                    )}
-            </div>
+            
             <div>
                 <SensorDigital/>
             </div>
--- src/components/Temperatura/SensorTem.jsx
+++ src/components/Temperatura/SensorTem.jsx
@@ -1,13 +1,44 @@
 import Style from "./SensorTem.module.css"
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export const SensorTemp = (props) => {
+    const [ data, setData ]= useState(null);
+    const API_OPC = "http://192.168.0.150:5005/opcua/listaValores";
+
+    useEffect(()=>{
+        async function getData() {
+                try {
+                const response = await axios.get(API_OPC);
+                    setData(response.data);
+                } catch (error) {
+                console.error(error);
+                }
+            }
+            const intervalId = setInterval(getData, 1000);
+
+    // Limpiar intervalo al desmontar el componente
+
+    return () => clearInterval(intervalId);
+
+    },[]) 
 
     return(
         <>
-            <div className={Style.boxSensor}>
-                <p className={Style.boxTitle}>Sensor Temperatura</p>
-                <h2 className={Style.boxCant}>{props.valor}<span>°C</span></h2>
+
+            <div>
+                {data && (
+                            <ul>
+                            {data.map((item, index) => (
+                                <div className={Style.boxSensor}>
+                                    <p className={Style.boxTitle}>Sensor Temperatura</p>
+                                    <h2 key={index} className={Style.boxCant}>{props.valor}<span>{item.data}°C</span></h2>
+                                </div>
+                            ))}
+                            </ul>
+                        )}
             </div>
+            
         </>
     )
 }
